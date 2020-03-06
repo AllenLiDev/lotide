7b4c2012955453f54f279119a470d466c15ce433
headhuntar@gmail.com <headhuntar@gmail.com>
2020-03-06 22:06:12 +0000

    added eqArray with recursion

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,23 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return arr1 === arr2;
+  }
+  if (arr1.length != arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+    if (!eqArrays(arr1[i], arr2[i])) {
+      return false;
+    }
   }
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 4], [1, 2, 4]), true); // => should PASS
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false\
-
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true); // => should PASS
+// nested arrays tests
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
