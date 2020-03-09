d41559e20084600fdd904bca5e77735ee95e1b22
headhuntar@gmail.com <headhuntar@gmail.com>
2020-03-09 16:45:28 +0000

    using module exports to practice DRY and creating new test files.

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`🏀🏀🏀Assertion Passed: ${actual} === ${expected} `);
   } else {
@@ -7,6 +7,4 @@ const assertEqual = function(actual, expected) {
   return;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🏀🏀🏀Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🔕🔕🔕Assertion Failed: ${actual} !== ${expected}`);
-  }
-  return;
-};
+const assertEqual = require('./assertEqual');
 
-const head = function(array) {
+const head = function (array) {
   if (array === undefined || array[0] === undefined) {
     return undefined;
   }
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(), undefined);
+module.exports = head;
